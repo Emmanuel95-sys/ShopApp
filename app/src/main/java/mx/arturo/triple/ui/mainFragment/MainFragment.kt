1856f45416c8dda72dc053cde8487e3a4882da7c
Emmanuel Peña <1em.ppe.35@gmail.com>
2020-08-11 23:11:58 -0500

    duplicate items bug resolved

--- app/src/main/java/mx/arturo/triple/ui/mainFragment/MainFragment.kt
+++ app/src/main/java/mx/arturo/triple/ui/mainFragment/MainFragment.kt
@@ -82,7 +82,7 @@ class MainFragment : Fragment() {
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
-            R.id.clear_data -> clearData()
+//            R.id.clear_data -> clearData()
             R.id.call_web_service -> callWebService()
         }
         return true
